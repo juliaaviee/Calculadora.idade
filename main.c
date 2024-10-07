691262a70f4aacc8db30e56ee21381373d58d956
Júlia Barroso Vieira <144620692+juliaaviee@users.noreply.github.com>
2024-10-07 15:57:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 //código calcular idade
-//*Aluna: Júlia Barroso Vieira
 int main()
 {
     int anoNasc;
